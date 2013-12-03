d906ee45c3b9695633e9f3adb4aac77d59f22ac9
Alexandar Drajev <a.drajev@MMAdmins-Mac-mini-98.local>
2013-12-03 17:20:54 +0200

    Create project

---
+++ 4-Home-Articles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  4-Home-Articles
+//
+//  Created by Alexandar Drajev on 12/3/13.
+//  Copyright (c) 2013 Alexander Drazhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 4-Home-ArticlesTests/__Home_ArticlesTests.m
@@ -0,0 +1,34 @@
+//
+//  __Home_ArticlesTests.m
+//  4-Home-ArticlesTests
+//
+//  Created by Alexandar Drajev on 12/3/13.
+//  Copyright (c) 2013 Alexander Drazhev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __Home_ArticlesTests : XCTestCase
+
+@end
+
+@implementation __Home_ArticlesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
